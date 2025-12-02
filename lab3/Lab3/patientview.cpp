4970fb1a51c78a08394c974e56f8e06002d6a5c5
chenweifeng <2753154660@qq.com>
2025-12-02 11:21:35 +0800

    ui设计,切换框架

---
+++ lab3/Lab3/patientview.cpp
@@ -0,0 +1,38 @@
+#include "patientview.h"
+#include "ui_patientview.h"
+
+PatientView::PatientView(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::PatientView)
+{
+    ui->setupUi(this);
+}
+
+PatientView::~PatientView()
+{
+    delete ui;
+}
+
+void PatientView::on_btSearch_clicked()
+{
+
+}
+
+
+void PatientView::on_btAdd_clicked()
+{
+    emit goPatientEditView();
+}
+
+
+void PatientView::on_btDelete_clicked()
+{
+
+}
+
+
+void PatientView::on_btEdit_clicked()
+{
+
+}
+
